8326d27fc0e962198e3d0dcc4b52b5a0c0f89786
H3w9VH3F4L4Tq5MbsfRi <rizezir@gmail.com>
2024-03-12 10:11:17 +0100

    Tutorial 1

--- main.cpp
+++ main.cpp
@@ -2,4 +2,33 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-int main() { return 0; }
+int main() { 
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "MKMG", NULL, NULL);
+    if (window == NULL) {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+    glViewport(0, 0, 800, 600);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0; 
+}
