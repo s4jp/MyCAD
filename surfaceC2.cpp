dfa4db7620be294cb54d5a17f07314ac1f870510
H3w9VH3F4L4Tq5MbsfRi <rizezir@gmail.com>
2024-08-30 00:21:05 +0200

    C2 cylinder generator working

--- surfaceC2.cpp
+++ surfaceC2.cpp
@@ -45,47 +45,48 @@ std::vector<Figure*> SurfaceC2::CalculatePlane(int cpCount, int segmentCountLoc,
 
 std::vector<Figure*> SurfaceC2::CalculateCylinder(int cpCount, int segmentCountLoc, int segmentIdxLoc, int divisionLoc, int otherAxisLoc, int xSegments, int zSegments, float radius, float height)
 {
-	float patchRadius = 2 * M_PI / xSegments;
-	float patchRadiusStep = patchRadius / 3.f;
-	float patchHeight = height / zSegments;
-	float patchHeightStep = patchHeight / 3.f;
-	float patchHeightOverlap = 2.f * patchHeightStep;
+	float segmentRadius = M_PI * 2.f / xSegments;
+	int heightSegmentCount = zSegments + 2;
+	float segmentHeight = height / heightSegmentCount;
 
 	glm::vec3 position = this->GetPosition();
 	std::vector<Figure*> newPoints = std::vector<Figure*>();
 
 	std::vector<glm::vec2> xzPositions = std::vector<glm::vec2>();
-	for (int i = 0; i < xSegments * 3; i++) {
-		float x = position.x + radius * glm::cos(i * patchRadiusStep);
-		float z = position.z + radius * glm::sin(i * patchRadiusStep);
+	for (int i = 0; i < xSegments; i++) {
+		float x = position.x + radius * glm::cos(i * segmentRadius);
+		float z = position.z + radius * glm::sin(i * segmentRadius);
 		xzPositions.push_back(glm::vec2(x, z));
 	}
 
 	for (int i = 0; i < zSegments; i++) {
 		for (int j = 0; j < xSegments; j++) {
-			bool firstRow = (i == 0);				// generate 4 right cps
-			bool firstColumn = (j == 0);			// generate 4 top cps
+			bool firstInRow = (j == 0);
+			bool firstInColumn = (i == 0);
 
 			std::vector<Figure*> cps = std::vector<Figure*>();
 			for (int k = 0; k < 16; k++) {
-				if (j == xSegments - 1 && k % 4 != 0) {
-					cps.push_back(patches[i * xSegments]->GetControlPoints()[k - 1]);
+				int columnIdx = j + k % 4;
+				if (columnIdx >= xSegments) {
+					if (j == 0)
+						cps.push_back(cps[k - 3]);
+					else
+						cps.push_back(patches[i * xSegments]->GetControlPoints()[columnIdx - xSegments + glm::floor(k / 4) * 4]);
 					continue;
 				}
-				if (!firstColumn && firstRow && k % 4 != 3) {
+				if (!firstInRow && k % 4 != 3) {
 					cps.push_back(patches[i * xSegments + (j - 1)]->GetControlPoints()[k + 1]);
 					continue;
 				}
-				if (!firstRow && firstColumn && glm::floor(k / 4) != 3) {
+				if (!firstInColumn && glm::floor(k / 4) != 3) {
 					cps.push_back(patches[(i - 1) * xSegments + j]->GetControlPoints()[k + 4]);
 					continue;
 				}
 
-				// TODO: fix center generation
-				float x = xzPositions[j * 3 + k % 4].x;
-				float y = position.y + patchHeight * (i - zSegments / 2.f) + glm::floor(k / 4) * patchHeightStep - i * patchHeightOverlap;
-				float z = xzPositions[j * 3 + k % 4].y;
-				Point* p = new Point(glm::vec3(x, position.y, z), 0.02F);
+				float x = xzPositions[columnIdx].x;
+				float y = position.y + (i + glm::floor(k / 4) - heightSegmentCount / 2.f) * segmentHeight;
+				float z = xzPositions[columnIdx].y;
+				Point* p = new Point(glm::vec3(x, y, z), 0.02F);
 				cps.push_back(p);
 				newPoints.push_back(p);
 			}
