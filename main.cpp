9e03f23f31c7af60a7d50cd6a9005b4b4da22adc
s4jp <rizezir@gmail.com>
2025-04-01 09:43:40 +0200

    Add MyCAD project and update references
    - Introduced a new project named "MyCAD" in the solution file.
    - Updated the project file to change the root namespace and project name from "MKMG" to "MyCAD".
    - Modified the GLFW window title in main.cpp to reflect the new project name.

--- main.cpp
+++ main.cpp
@@ -140,7 +140,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(width, height, "MKMG", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(width, height, "MyCAD", NULL, NULL);
     if (window == NULL) {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
